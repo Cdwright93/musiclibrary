64c0fa19096c7952c972d2e8861a970103ef7952
Cdwright93 <80175712+whatupbrownbear@users.noreply.github.com>
2021-05-20 14:47:05 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = axios.create({
+  baseURL:'http://127.0.0.1:8000/music/'
+})
+class App extends Component {
+  
+  state = {
+    music: []
+  }
+
+  constructor(){
+    super();
+    this.getMusic()
+  }
+
+  getMusic = async () => {
+    let data = await api.get('/').then(({ data }) => data)
+      this.setState({ music : data })
+      console.log(this.state.music)
+  }
+  createSong = async () => {
+    let res = await api.post('/', {"title": "BLEACH",
+    "artist": "BROCKHAMPTON",
+    "album": "SATURATION III",
+    "release_date": "2017-04-22"})
+    console.log(res)
+    this.getMusic();
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  } 
 }
 
 export default App;
