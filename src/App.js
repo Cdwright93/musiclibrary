0b576f383b06187da33e376fa79d131b273cc8aa
Cdwright93 <80175712+whatupbrownbear@users.noreply.github.com>
2021-05-24 23:00:57 -0400

    Properly adding to the db

--- src/App.js
+++ src/App.js
@@ -6,31 +6,34 @@ const api = axios.create({
   baseURL:'http://127.0.0.1:8000/music/'
 })
 class App extends Component {
-  
-  state = {
-    music: []
-  }
-
   constructor(){
     super();
+    this.state = {
+      music: [],
+    }
     this.getMusic()
   }
-
+  
   getMusic = async () => {
     let data = await api.get('/').then(({ data }) => data)
-      this.setState({ music : data })
-      console.log(this.state.music)
+    this.setState({ music : data })
+    console.log(this.state.music)
   }
-  createSong = async () => {
-    let res = await api.post('/', {"title": " ",
-    "artist": " ",
-    "album": " ",
-    "release_date": " "})
+  createSong = async (event) => {
+    event.preventDefault()
+    let res = await api.post('/', {title: event.target.title.value,
+    artist: event.target.artist.value,
+    album: event.target.album.value,
+    release_date: event.target.release_date.value})
     console.log(res)
     this.getMusic();
   }
   deleteSong = async (id) => {
     let data = await api.delete(`/${id}`)
+    this.getMusic()
+  }
+  updateSong = async (id, val) => {
+    let data = await api.put(`/${id}`, { title: val }, {artist : val}, {album : val}, {release_date : val})
     this.getMusic()
   }
   render() {
@@ -44,8 +47,21 @@ class App extends Component {
           <td><h3>Album</h3></td>
           <td><h3>Release Date</h3></td>
           </thead>
-          {this.state.music.map(song => <tr key={song.id}><td>{song.title}</td> <td>{song.artist}</td>  <td>{song.album}</td> <td>{song.release_date}</td></tr>)}
+          {this.state.music.map(song => <tr key={song.id}><td>{song.title}</td> <td>{song.artist}</td><td>{song.album}</td> <td>{song.release_date}</td><button onClick={()=>this.deleteSong(song.id)}>Delete</button>
+          <button onClick = {() => this.updateSong(song.id, )}>Edit</button></tr>)}
         </table>
+        <form onSubmit = {(event) => this.createSong(event)}>
+          <h3>Add a song</h3>
+        <label htmlfor="title">Title:</label>
+        <input type = "text" id="title" name="title"/><br/>
+        <label htmlfor="artist">Artist:</label>
+        <input type = "text" id="artist" name="artist"/><br/>
+        <label htmlfor="album">Album:</label>
+        <input type = "text" id="album" name="album"/><br/>
+        <label htmlfor="release_date">Release Date:</label>
+        <input type="text" id="release_date" name="release_date"/><br/>
+        <button type="submit">Submit</button>
+        </form>
       </div>
     );
   } 
